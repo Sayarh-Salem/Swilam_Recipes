bbc4a12e4b7de79e1452d7cc47f1e07c57d5546a
Salem-Sayarh <91844209+Sayarh-Salem@users.noreply.github.com>
2022-03-05 19:40:42 +0100

    Add files via upload

---
+++ Jscript.js
@@ -0,0 +1,21 @@
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+
+  if (element) return element;
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  );
+};
+
+// Navbar
+const links = getElement(".nav-links");
+const navBtnDOM = getElement(".nav-btn");
+
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("menu-show");
+});
+
+//Footer date
+const date = getElement("#date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
